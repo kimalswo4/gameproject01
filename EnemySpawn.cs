dac2325233e7db26d555da29af64d67434153513
unknown <>
2016-04-02 12:42:08 +0900

    타워디펜스 적군나오는것은 구현완료 타워 능력들 표현하는 것은아직

---
+++ EnemySpawn.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class EnemySpawn : MonoBehaviour {
+
+    public GameObject NormalPrefab;
+    public GameObject HeavyPrefab;
+
+    public int enemiesLeft;
+    public float spawnTime;
+    public float spawnTimeLeft;
+    public float spawnTimeSeconds = 2f;
+    public int i = 0;
+    public int[] waveFormation;
+
+    void Start()
+    {
+        //waveFormation = new int[] {};
+        enemiesLeft = waveFormation.Length;
+    }
+
+    void Update()
+    {
+        spawnTimeLeft = Time.time - spawnTime;
+        if(spawnTimeLeft >= spawnTimeSeconds)
+        {
+            if(i != waveFormation.Length)
+            {
+                if(waveFormation[i]==0)
+                {
+                    Instantiate(NormalPrefab, transform.position, Quaternion.identity);
+                    spawnTime = Time.time;
+                    spawnTimeLeft = 0;
+                    i++;
+                }
+                else
+                {
+                    Instantiate(HeavyPrefab, transform.position, Quaternion.identity);
+                    spawnTime = Time.time;
+                    spawnTimeLeft = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+}
---
+++ golem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class golem : MonoBehaviour {
+
+    public Transform Enemy;
+    public float  check;
+
+    Queue que = new Queue();
+    
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    
+}
