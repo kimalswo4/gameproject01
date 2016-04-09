55dd726e94efded6ce41d8a30bf90df0a892ec2c
unknown <>
2016-04-09 23:23:43 +0900

    현재 카메라를통해서 타워를 설치할수있도록 작업을해놓음

--- CameraManeger.cs
+++ CameraManeger.cs
@@ -5,7 +5,13 @@ public class CameraManeger : MonoBehaviour {
     private Vector3 ResetCamera;
     private Vector3 Origin;
     private Vector3 Diference;
-
+    private bool tower1 = false;
+    private bool tower2 = false;
+    private bool tower3 = false;
+    public GameObject Tower1;
+    public GameObject Tower2;
+    public GameObject Tower3;
+    private bool Groundcheck;
 
 
     private bool Drag = false;
@@ -13,19 +19,29 @@ public class CameraManeger : MonoBehaviour {
     void start()
     {
         ResetCamera = Camera.main.transform.position;
+        Groundcheck = GetComponent<GroundScript>().build;
+    }
 
+
+    void Update()
+    {
+        check();
     }
 
     void LateUpdate() //LateUpdate()는 Update() 후 프레임마다 한 번씩 호출됩니다. Update()에서 수행되는 계산이 완료되면 LateUpdate() 함수가 시작 합니다. 
     {
         if (Input.GetMouseButton(0))
         {
+
+           
             Diference = (Camera.main.ScreenToWorldPoint(Input.mousePosition)) - Camera.main.transform.position;
             if (Drag == false)
             {
                 Drag = true;
                 Origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             }
+
+            
         }
         else
         {
@@ -42,4 +58,40 @@ public class CameraManeger : MonoBehaviour {
         }
 
     }
+
+   void check()
+   {
+       RaycastHit hit = new RaycastHit();
+
+       Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       if(Input.GetMouseButtonDown(0))
+       { 
+           if (Physics.Raycast(ray, out hit))
+           {
+               if (hit.collider.tag == "Tower1")
+               {
+                   tower1 = true;
+                   Debug.Log("check");
+               }
+
+               if(hit.collider.tag == "Tower2")
+               {
+                   tower2 = true;
+               }
+           
+               if(hit.collider.tag == "Tower3")
+               {
+                   tower3 = true;
+               }
+               if(hit.collider.tag == "Ground" && tower1 == true && Groundcheck == false)
+               {
+                   
+                   Instantiate(Tower1, hit.transform.position, Quaternion.identity);
+                   Groundcheck = true;
+                   tower1 = false;
+               }
+               
+           }
+       }
+   }
 }
