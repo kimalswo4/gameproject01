194c5d2a85c7cf87874cd3ff3627bad20bfd33e3
unknown <>
2016-04-14 23:39:27 +0900

    전체적으로 코드를손보는중입니다.

--- golem.cs
+++ golem.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class golem : MonoBehaviour {
+public class golem : MonoBehaviour
+{
 
     public float Range = 2.5f; //공격범위
-    
-    
+
+
     public float SearchTimeLeft = 0.0f;
     public float SearchTimeSeconds = 2.0f;
 
@@ -18,49 +19,69 @@ public class golem : MonoBehaviour {
     private GameObject Enemy;
     public Vector3 targetDir;
 
-	// Use this for initialization
-	void Start () {
+    public bool S_Debuff;
+    private bool R_Debuff;
+
+    // Use this for initialization
+    void Start()
+    {
         _transform = this.transform;
         _Attackrange = new Vector3(Range, Range);
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        S_Debuff = false;
+    }
 
-        RaycastHit[] hit = Physics.BoxCastAll(_transform.position, _Attackrange, Vector3.forward, Quaternion.identity, Mathf.Infinity, (-1) - (1 << LayerMask.NameToLayer("Tower")));
-        
-        //RaycastHit2D[] hit = Physics2D.BoxCastAll(_transform.position, _Attackrange, 0, Vector2.zero);
-        if (Time.time > SearchTimeLeft) 
+    // Update is called once per frame
+    void Update()
+    {
+        if (S_Debuff == true && R_Debuff == true)
         {
-            
-            SearchTimeLeft = Time.time + SearchTimeSeconds;
-            if (hit != null)
-            {
-                Debug.Log("hithithit!");
-                
-                    for (int num = 0; num < hit.Length; num++)
-                    {
-                        
-                        if (hit[num].collider.gameObject.tag == "Enemy" )
-                        {
-
-
-                            Debug.Log("check");
-                            //GameObject missile = Instantiate(Missile, MissilePoint.transform.position, MissilePoint.transform.rotation)as GameObject;
-                            //missile.GetComponent<Missile>().SetTarget(hit[num].collider.transform);
-                            //missile.GetComponent<Missile>().SetTower(MissilePoint.transform);                       
-                            GameObject missile = Instantiate(Missile, transform.position, MissilePoint.transform.rotation) as GameObject;
-                            missile.GetComponent<Missile>().SetTarget(hit[num].collider.transform);
-                            missile.GetComponent<Missile>().SetTower(MissilePoint.transform);
-                                
-                        }
-                    }
-                
-            }
-
+            Invoke("ChangeBuff", 5);
+            R_Debuff = false;
         }
-                
-	}
-    
+        CheckEnemyShot();
+
+    }
+
+    void CheckEnemyShot()
+    {
+        RaycastHit[] hit = Physics.BoxCastAll(_transform.position, _Attackrange, Vector3.forward, Quaternion.identity, Mathf.Infinity, (-1) - (1 << LayerMask.NameToLayer("Tower")));
+        //RaycastHit2D[] hit = Physics2D.BoxCastAll(_transform.position, _Attackrange, 0, Vector2.zero);
+
+        if (Time.time <= SearchTimeLeft)
+            return;
+        SearchTimeLeft = Time.time + SearchTimeSeconds;
+        if (hit == null)
+            return;
+        int count = 0;//
+        for (int num = 0; num < hit.Length; num++)
+        {
+
+            if (hit[num].collider.gameObject.tag == "Enemy")
+            {
+
+                if (S_Debuff == false)
+                {
+                    if (count > 4)
+                        return;
+                    count++;
+                    //GameObject missile = Instantiate(Missile, MissilePoint.transform.position, MissilePoint.transform.rotation)as GameObject;
+                    //missile.GetComponent<Missile>().SetTarget(hit[num].collider.transform);
+                    //missile.GetComponent<Missile>().SetTower(MissilePoint.transform);                       
+                    Missile missile = Instantiate(Missile, transform.position, MissilePoint.transform.rotation) as Missile;
+                    missile.SetTarget(hit[num].collider.transform);
+                    missile.SetTower(MissilePoint.transform);
+                    missile.SetDamage(2);
+                    //missile.SetImage(0);
+                }
+            }
+        }
+
+    }
+
+    void ChangeBuff()
+    {
+
+        S_Debuff = !S_Debuff;
+        R_Debuff = true;
+    }
 }
